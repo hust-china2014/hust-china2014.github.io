000bf40e2d4951b4805f23d16cc422ab99f191b9
husteiac <hust.why@gmail.com>
2014-10-17 18:37:08 +0800

    a
    a

---
+++ js/main.js
@@ -0,0 +1,6 @@
+window.onload = function() {
+    $(".parallax__layer__back").addClass("parallax__layer__back__after");
+    $(".parallax__layer__back").removeClass("parallax__layer__back");
+
+};
+
